dd437061b99e49b61b994aac1022710c78dc2105
macieksm19 <maciek.smieja05@icloud.com>
2025-03-13 20:52:52 +0100

    Add user input for number of top charted songs to display

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,18 @@ package org.example;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in);
         DatabaseOperations databaseOperations = new DatabaseOperations();
         JSONObject database = databaseOperations.getJSONObject(databaseOperations.connect());
         ChartOperations chartOperations = new ChartOperations(databaseOperations, database);
 
-        ArrayList<Song> topChartedSongs = chartOperations.getTopChartedSongs(5, chartOperations.getSongs());
-        topChartedSongs.forEach(song -> System.out.println(song.getThisWeek() + ". " + song.getTitle() + " - " + song.getArtist()));
-
-        topChartedSongs = chartOperations.getTopChartedSongs(10, chartOperations.getSongs());
+        System.out.print("How many top charted songs would you like to see? ");
+        int input = scanner.nextInt();
+        ArrayList<Song> topChartedSongs = chartOperations.getTopChartedSongs(input, chartOperations.getSongs());
         topChartedSongs.forEach(song -> System.out.println(song.getThisWeek() + ". " + song.getTitle() + " - " + song.getArtist()));
     }
 }
